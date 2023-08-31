05fc3cffc307580768f7c9cb613610af3a92c287
Casey <87348212+2cheinz@users.noreply.github.com>
2023-08-31 05:43:35 -0500

    Get random computer choice generator working

--- rps.js
+++ rps.js
@@ -1,7 +1,17 @@
+// Create a function to randomly generate a choice for the computer using Math.random()
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
 
+    if (computerChoice === 1) {
+        console.log(`Computer chooses Rock`);
+    } else if (computerChoice === 2) {
+        console.log('Computer chooses Paper');
+    } else {
+        console.log('Computer chooses Scissors');
+    }
+}
 
-
-
+getComputerChoice()
 
 
 
