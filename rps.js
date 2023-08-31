59427aa1753142230ebfff8ccbf9660ad5d1631e
Casey <87348212+2cheinz@users.noreply.github.com>
2023-08-31 05:58:50 -0500

    Play round function created

--- rps.js
+++ rps.js
@@ -4,14 +4,35 @@ function getComputerChoice() {
 
     if (computerChoice === 1) {
         console.log(`Computer chooses Rock`);
+        return 1;
     } else if (computerChoice === 2) {
         console.log('Computer chooses Paper');
+        return 2;
     } else {
         console.log('Computer chooses Scissors');
+        return 3;
     }
 }
 
-getComputerChoice()
+function playRound (computerSelection, playerSelection){
+    if (computerSelection === 1 && playerSelection === 'rock' || computerSelection === 2 && playerSelection === 'paper' || computerSelection === 3 && playerSelection === 'scissors'){
+        console.log('It is a tie');
+    }
+    else if (computerSelection === 1 && playerSelection === 'scissors' || computerSelection === 2 && playerSelection === 'rock' || computerSelection === 3 && playerSelection === 'paper'){
+        console.log('Computer wins!');
+    }
+    else {
+        console.log('Player wins!')
+    }
+}
+
+let computerSelection = getComputerChoice();
+let playerSelection = 'rock';
+
+console.log(`User selects ${playerSelection}`)
+console.log(computerSelection)
+
+playRound(computerSelection, playerSelection);
 
 
 
