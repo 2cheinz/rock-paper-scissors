9a4745a3ad60bfbb0f4dafc01ec9aa2285e45f38
Casey <87348212+2cheinz@users.noreply.github.com>
2023-09-01 07:28:12 -0500

    Re-work round & game functions

--- rps.js
+++ rps.js
@@ -1,38 +1,61 @@
+let playerScore = 0
+let computerScore = 0
+
 // Create a function to randomly generate a choice for the computer using Math.random()
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
 
     if (computerChoice === 1) {
-        console.log(`Computer chooses Rock`);
-        return 1;
+        computerChoice = 'ROCK';
+        console.log(computerChoice);
+        return computerChoice;
     } else if (computerChoice === 2) {
-        console.log('Computer chooses Paper');
-        return 2;
+        computerChoice = 'PAPER';
+        console.log(computerChoice);
+        return computerChoice;
     } else {
-        console.log('Computer chooses Scissors');
-        return 3;
+        computerChoice = 'SCISSORS';
+        console.log(computerChoice);
+        return computerChoice;
     }
 }
 
+// Create a function to generate user's input to being case insensitive
+function getUserChoice(weapon){
+    return weapon.toUpperCase();
+}
+
+// Play one round of RPS
 function playRound (computerSelection, playerSelection){
-    if (computerSelection === 1 && playerSelection === 'rock' || computerSelection === 2 && playerSelection === 'paper' || computerSelection === 3 && playerSelection === 'scissors'){
-        console.log('It is a tie');
+    console.log(computerSelection);
+    console.log(playerSelection);
+    if (computerSelection === 'ROCK' && playerSelection === 'ROCK' || computerSelection === 'PAPER' && playerSelection === 'PAPER' || computerSelection === 'SCISSORS' && playerSelection === 'SCISSORS'){
+        return('It is a tie');
     }
-    else if (computerSelection === 1 && playerSelection === 'scissors' || computerSelection === 2 && playerSelection === 'rock' || computerSelection === 3 && playerSelection === 'paper'){
-        console.log('Computer wins!');
+    else if (computerSelection === 'ROCK' && playerSelection === 'SCISSORS' || computerSelection === 'PAPER' && playerSelection === 'ROCK' || computerSelection === 'SCISSORS' && playerSelection === 'PAPER'){
+        computerScore++;
+        return('Computer wins!');
     }
     else {
-        console.log('Player wins!')
+        playerScore++;
+        return('Player wins!');
+
     }
 }
 
-let computerSelection = getComputerChoice();
-let playerSelection = 'rock';
+function game(){
+    let playerSelection = prompt('Make your selection', 'Rock');
+    playerSelection = getUserChoice(playerSelection);
+    let computerSelection = getComputerChoice();
+    console.log(`User selects ${playerSelection}`);
+    console.log(`Computer selects ${computerSelection}`);
+    let roundResult = playRound(computerSelection, playerSelection);
+    console.log(roundResult);
+    console.log(playerScore);
+    console.log(computerScore);
+}
 
-console.log(`User selects ${playerSelection}`)
-console.log(computerSelection)
-
-playRound(computerSelection, playerSelection);
+game();
 
 
 
